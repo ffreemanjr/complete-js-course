52f7c19b3c64d82ad5f88f3f244adf68b90112da
foyah.genesistechs@gmail.com <foyah.genesistechs@gmail.com>
2019-01-09 00:59:52 -0500

    practicing variable declaration

---
+++ 2-JS-basics/script.js
@@ -0,0 +1,25 @@
+//================ CREATING VARIABLES =============== 
+
+var firstName = 'Foyah';
+console.log(firstName);
+
+var lastName = 'Freeman';
+var age = 49;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job; //This will produce an "undefined" type because it was defined with no value assigned.
+console.log(job); 
+
+job = 'Teacher';
+console.log(job);
+
+
+var _13years = 3;
+console.log(_13years);
+
+var johnMark = 'John and Mark';
+console.log(johnMark);
+
+
