8c7b949674ae008493fb863ba7ec130e7793e729
foyah.genesistechs@gmail.com <foyah.genesistechs@gmail.com>
2019-01-10 15:01:10 -0500

    rules for creating variables

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,5 +1,5 @@
 //================ CREATING VARIABLES =============== 
-
+/*
 var firstName = 'Foyah';
 console.log(firstName);
 
@@ -15,11 +15,23 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
-
+// VARIABLE NAMING RULES
 var _13years = 3;
 console.log(_13years);
 
 var johnMark = 'John and Mark';
 console.log(johnMark);
+*/
+
+
+// ============== VARIABLE MUTATION ======================
+var firstName = 'Foyah';
+var age = 49;
+console.log(firstName + ' is ' + age + '.'); //Using type coersion, javaScript will convert one data type to another.
+
+var job, isMarried;
+job = 'Developer';
+isMarried = true;
+console.log(firstName + ' is a ' + age + ' year old ' + job +'.' + ' Is he married? ' + isMarried);
 
 
